c45c9a84b2c03de6dec4ed8d555680e080e610a1
real-zephex <zephex-1@outlook.com>
2025-02-16 08:56:45 +0530

    Remove ProfileButton component from SidebarT

--- src/components/ui/sidebar/page.tsx
+++ src/components/ui/sidebar/page.tsx
@@ -1,14 +1,9 @@
-import ProfileButton from "../profileButton";
 import Sidebar from "../sidebar";
 
 const SidebarT = async () => {
   return (
     <main>
       <Sidebar />
-
-      <div className="z-20">
-        <ProfileButton />
-      </div>
     </main>
   );
 };
